2d4bff22701aa03e0bb1b5858e3d565244ea791d
AidanMiddel <70641287+AidanMiddel@users.noreply.github.com>
2022-11-10 21:24:21 +0100

    video 3

--- src/Question/Question.js
+++ src/Question/Question.js
@@ -3,12 +3,16 @@ import "./Question.css"
 const Question = () => {
     return (
         <article className="question">
-            <header className="question__hearder">
+            <header className="question__header">
                 <h2>Hoe goed beheers je nu react?</h2>
             </header>
             <section className="question__section">
                 ⭐⭐⭐⭐⭐
             </section>
+            <footer className="question__footer">
+                <button className="question__previous">Vorige</button>
+                <button className="question__next">Volgende</button>
+            </footer>
         </article>
     )
 }
--- videos.md
+++ videos.md
@@ -1,6 +1,5 @@
 Video's Les 9 - React-router, meerdere URL's, het onthouden van grotere stukken data
 
-Video 3/23 => https://web.microsoftstream.com/video/df2eb953-325c-4388-ae47-af1346b23af8?list=studio
 Video 4/23 => https://web.microsoftstream.com/video/cc7aa1f6-82a9-4de1-b20a-74955a684f1c?list=studio
 Video 5/23 => https://web.microsoftstream.com/video/6ab67705-a65a-4a27-b868-bb215bcfdfe5?list=studio
 Video 6/23 => https://web.microsoftstream.com/video/944299ba-d1c5-4647-abff-1c7bf3e5c006?list=studio
